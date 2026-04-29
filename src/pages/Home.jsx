8f6f362ef871a2bdaa8258f78d9f2ca59fac350c
Cristian Casanova <cristian_casanovaan@fet.edu.co>
2026-04-28 23:56:21 -0500

    Mejora del diseño / Responsive Implementado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -150,7 +150,7 @@ export default function Home() {
                             </div>
                         ) : (
                             <div className="flex justify-center py-10">
-                                <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-[#610361]"></div>
+                                 <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-[#610361]"></div>
                             </div>
                         )}
                     </div>
