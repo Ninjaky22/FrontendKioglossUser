4f4f73066e5e317704d060a7f2e7b9f7aa67aa9f
Cristian Casanova <cristian_casanovaan@fet.edu.co>
2026-04-29 23:54:48 -0500

    Mejoras Diseño / Responsive Implementado

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import Breadcrumb from '../components/Breadcrumb';
 export default function Contact() {
     return (
         <div className="bg-[#F7E6FE font-winkySans">
-            <Breadcrumb items={[
+            <Breadcrumb items={[ 
                 { label: 'Inicio', path: '/', icon: 'fa-solid fa-house text-[#610361]' },
                 { label: 'Contacto' },
             ]} />
